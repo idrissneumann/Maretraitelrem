fd2eb8aaadca0b9d7191dc5c0da95323a4e479e1
Idriss Neumann <idriss.neumann@comwork.io>
2024-06-28 11:32:53 +0100

    Fix script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,17 @@ function updateResults() {
 
     // Au bout du compte c'est ce qu'on veux haha
     const difference = pension >= 0 ? -1 * pension : pension;
-    const pensionNet1 = pension + difference;
-    const pensionNet2 = difference;
+    const pensionNet = 0;
 
     document.querySelector(".alert").style.color =
       difference < 0 ? "#EE2C17" : "#000";
 
     document.getElementById("win_lose").textContent =
-      difference < 0 ? "perdue" : "gagnée";
+      difference < 0 ? "perdue" : "gagn&eacute;e";
 
-    document.getElementById("pensionNet1").textContent = pensionNet1.toFixed(0);
-    document.getElementById("pensionNet2").textContent = pensionNet2.toFixed(0);
+    document.getElementById("pensionNet").textContent = pensionNet.toFixed(0);
     document.getElementById("difference").textContent = difference.toFixed(0);
   } else {
-    document.getElementById("pensionNet1").textContent = "";
     document.getElementById("pensionNet2").textContent = "";
     document.getElementById("difference").textContent = "";
   }
